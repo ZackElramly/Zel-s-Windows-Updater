5ebd04d2cc0671b02e6ab24f7d1f0fcd249b2d57
Zackie Elramly <61506753+ZackElramly@users.noreply.github.com>
2025-12-14 00:17:54 -0500

    Add output display for Windows update commands

--- winupdate.ps1
+++ winupdate.ps1
@@ -2,8 +2,8 @@ Start-Process powershell -Verb RunAs -ArgumentList @(
     '-NoExit',
     '-Command',
     '& {
-        Install-Module PSWindowsUpdate -Force -Confirm:$false
-        Import-Module PSWindowsUpdate
-        Get-WindowsUpdate -Install -AcceptAll -AutoReboot
+        Install-Module PSWindowsUpdate -Force -Confirm:$false | Out-Host
+        Import-Module PSWindowsUpdate | Out-Host
+        Get-WindowsUpdate -Install -AcceptAll -AutoReboot | Out-Host
     }'
 )
