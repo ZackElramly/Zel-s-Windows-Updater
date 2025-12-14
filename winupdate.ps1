2ad4ec182a49dd9b0fe5d2fa02995fb1be4c22a4
Zackie Elramly <61506753+ZackElramly@users.noreply.github.com>
2025-12-14 00:15:28 -0500

    Refactor PowerShell script to use inline command

--- winupdate.ps1
+++ winupdate.ps1
@@ -1,14 +1,9 @@
-$cmd = @"
-Install-Module -Name PSWindowsUpdate -Force
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate -Install -AcceptAll -AutoReboot
-"@
-
-Start-Process powershell -Verb RunAs `
-  -ArgumentList "-NoExit -Command `$cmd"
-  #-ArgumentList "-NoExit -Command `$cmd"
-
-#Requires -RunAsAdministrator
-#Set-ExecutionPolicy RemoteSigned
-#Get-WindowsUpdate
-#Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+Start-Process powershell -Verb RunAs -ArgumentList @(
+    '-NoExit',
+    '-Command',
+    '& {
+        Install-Module PSWindowsUpdate -Force -Confirm:$false
+        Import-Module PSWindowsUpdate
+        Get-WindowsUpdate -Install -AcceptAll -AutoReboot
+    }'
+)
