6560fa621119bf58df7e8a6d07483b939822aa1d
Zackie Elramly <61506753+ZackElramly@users.noreply.github.com>
2025-12-14 00:26:36 -0500

    Remove redundant NuGet package provider installation

--- winupdate.ps1
+++ winupdate.ps1
@@ -2,8 +2,8 @@ Start-Process powershell -Verb RunAs -ArgumentList @(
     '-NoExit',
     '-Command',
     '& {
+        Install-PackageProvider -Name NuGet -Force | Out-Host
         Install-Module PSWindowsUpdate -Force -Confirm:$false | Out-Host
-        Install-PackageProvider -Name NuGet -Force
         Import-Module PSWindowsUpdate | Out-Host
         Get-WindowsUpdate -Install -AcceptAll -AutoReboot | Out-Host
     }'
