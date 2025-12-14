564fa10eef432bef826199ae3045b48c5e8f88c1
Zackie Elramly <61506753+ZackElramly@users.noreply.github.com>
2025-12-14 00:08:52 -0500

    Refactor winupdate.ps1 to use Start-Process

--- winupdate.ps1
+++ winupdate.ps1
@@ -1,7 +1,14 @@
-#Requires -RunAsAdministrator
-#Set-ExecutionPolicy RemoteSigned
+$cmd = @"
+Start-Process powershell -Verb RunAs
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 Get-WindowsUpdate -Install -AcceptAll -AutoReboot
+"@
+
+Start-Process powershell -Verb RunAs `
+  -ArgumentList "-NoExit -Command `$cmd"
+
+#Requires -RunAsAdministrator
+#Set-ExecutionPolicy RemoteSigned
 #Get-WindowsUpdate
 #Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
