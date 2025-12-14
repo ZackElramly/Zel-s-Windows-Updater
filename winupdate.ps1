58dce083a53e9d5dca028dc16fc988ce1df1bd87
Zackie Elramly <61506753+ZackElramly@users.noreply.github.com>
2025-12-14 00:29:30 -0500

    Add installation steps for NuGet and PSWindowsUpdate

--- winupdate.ps1
+++ winupdate.ps1
@@ -5,9 +5,13 @@ Start-Process powershell -Verb RunAs -ArgumentList @(
     '& {
         $ProgressPreference = ''Continue''
         $VerbosePreference  = ''Continue''
+        Write-Host "Installing NuGet provider..."
         Install-PackageProvider -Name NuGet -Force | Out-Host
+        Write-Host "Installing PSWindowsUpdate module..."
         Install-Module PSWindowsUpdate -Force -Confirm:$false | Out-Host
+        Write-Host "Importing module..."
         Import-Module PSWindowsUpdate | Out-Host
+        Write-Host "Starting Windows Update..."
         Get-WindowsUpdate -Install -AcceptAll -AutoReboot | Out-Host
     }'
 )
